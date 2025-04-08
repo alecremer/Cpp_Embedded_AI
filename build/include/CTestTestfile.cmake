6e6dc604632ff3e85f2af8a438c774169ae0fc01
alecremer <ale.cremer.dev@gmail.com>
2025-04-08 13:06:57 -0300

    house keeping

--- build/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ale/AI/include
-# Build directory: /home/ale/AI/build/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
